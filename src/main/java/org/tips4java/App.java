4d3ca6d24cb52e0e2bf92a7486a3d026940a97ba
kansasSamurai <kansasSamurai@users.noreply.github.com>
2024-02-05 19:59:39 -0600

    start maven project

---
+++ src/main/java/org/tips4java/App.java
@@ -0,0 +1,13 @@
+package org.tips4java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
